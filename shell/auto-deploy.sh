8b7d29ab0f504a47696b4021817a4a7ee16605a0
Arbor Huang <arbor.huang@gmail.com>
2018-03-10 23:37:43 +0800

    add cni bridge

--- shell/auto-deploy.sh
+++ shell/auto-deploy.sh
@@ -209,7 +209,7 @@ kube::config_cni()
     mkdir -p /etc/cni/net.d
 cat >/etc/cni/net.d/10-mynet.conf <<EOF
 {
-    "cniVersion": "0.3.0",
+    "cniVersion": "0.6.0",
     "name": "mynet",
     "type": "bridge",
     "bridge": "cni0",
@@ -226,7 +226,7 @@ cat >/etc/cni/net.d/10-mynet.conf <<EOF
 EOF
 cat >/etc/cni/net.d/99-loopback.conf <<EOF
 {
-    "cniVersion": "0.3.0",
+    "cniVersion": "0.6.0",
     "type": "loopback"
 }
 EOF
