16fd26c9c525fcb3b95bc95f5c6f1b45de96e771
isaron <isaron@sohu.com>
2018-04-09 16:19:16 +0800

    add del-old.sh

---
+++ shell/del-old.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -x
+dockerimages=(
+    kube-apiserver-amd64:v1.9.5
+    kube-controller-manager-amd64:v1.9.5
+    kube-scheduler-amd64:v1.9.5
+    kube-proxy-amd64:v1.9.5
+    etcd-amd64:3.1.12
+    pause-amd64:3.0
+    k8s-dns-sidecar-amd64:1.14.7
+    k8s-dns-kube-dns-amd64:1.14.7
+    k8s-dns-dnsmasq-nanny-amd64:1.14.7
+    kubernetes-dashboard-amd64:v1.8.3
+    cluster-autoscaler:v1.1.2
+    defaultbackend:1.4
+)
+
+j=1
+for i in ${dockerimages[@]}
+do
+    echo $i
+    echo $j
+
+    docker rmi arborhuang/$i
+    docker rmi k8s.gcr.io/$i
+    docker rmi gcr.io/google_containers/$i
+
+    let j+=1
+done
+
+set +x
