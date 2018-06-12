133ac2281a2ddec6f2bd14409fa249466e71ee58
isaron <isaron@sohu.com>
2018-06-12 17:41:59 +0800

    add jenkins-x images

--- shell/pull-gcr.sh
+++ shell/pull-gcr.sh
@@ -30,30 +30,6 @@ do
     let j+=1
 done
 
-set -x
-jximages=(
-    heapster:v1.3.0
-    heapster-amd64:v1.5.0
-    heapster-grafana-amd64:v4.4.3
-    heapster-influxdb-amd64:v1.3.3
-    kube-addon-manager:v8.6
-    addon-resizer:1.7
-)
-
-j=1
-for i in ${jximages[@]}
-do
-    echo $i
-    echo $j
-
-    docker pull arborhuang/$i
-    docker tag arborhuang/$i k8s.gcr.io/$i
-    docker save k8s.gcr.io/$i -o $i.tar
-    docker rmi arborhuang/$i
-
-    let j+=1
-done
-
 docker pull arborhuang/quay-nginx-ingress-controller:0.15.0
 docker tag arborhuang/quay-nginx-ingress-controller:0.15.0 quay.io/kubernetes-ingress-controller/nginx-ingress-controller:0.15.0
 docker save quay.io/kubernetes-ingress-controller/nginx-ingress-controller:0.15.0 -o nginx-ingress-controller:0.15.0.tar
@@ -115,6 +91,60 @@ do
     docker tag arborhuang/spinnaker-$i gcr.io/spinnaker-marketplace/$i
     docker save gcr.io/spinnaker-marketplace/$i -o spinnaker-$i.tar
     docker rmi arborhuang/spinnaker-$i
+
+    let j+=1
+done
+
+set -x
+jximages=(
+    heapster:v1.3.0
+    heapster-amd64:v1.5.0
+    heapster-grafana-amd64:v4.4.3
+    heapster-influxdb-amd64:v1.3.3
+    kube-addon-manager:v8.6
+    addon-resizer:1.7
+)
+
+j=1
+for i in ${jximages[@]}
+do
+    echo $i
+    echo $j
+
+    docker pull arborhuang/$i
+    docker tag arborhuang/$i k8s.gcr.io/$i
+    docker save k8s.gcr.io/$i -o $i.tar
+    docker rmi arborhuang/$i
+
+    let j+=1
+done
+
+set -x
+jximages2=(
+    chartmuseum/chartmuseum:v0.2.8
+    jenkinsxio/jenkinsx:0.0.25
+    jenkinsci/jnlp-slave:3.14-1
+    jenkinsxio/builder-maven:0.0.280
+    jenkinsxio/builder-gradle:0.0.163
+    jenkinsxio/builder-scala:0.0.96
+    jenkinsxio/builder-go:0.0.275
+    jenkinsxio/builder-terraform:0.0.52
+    jenkinsxio/builder-rust:0.0.128
+    jenkinsxio/builder-nodejs:0.0.228
+    jenkinsxio/builder-base:0.0.312
+    jenkinsxio/builder-python2:0.0.45
+    jenkinsxio/builder-python:0.0.46
+    jenkinsxio/builder-ruby:0.0.312
+    jenkinsxio/builder-swift:0.0.312
+)
+
+j=1
+for i in ${jximages2[@]}
+do
+    echo $i
+    echo $j
+
+    docker pull $i
 
     let j+=1
 done
