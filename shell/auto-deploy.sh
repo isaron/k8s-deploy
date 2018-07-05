0d09952a0d56b69c3cd2b543f4ae402b2c85c6e1
isaron <isaron@sohu.com>
2018-07-05 11:18:27 +0800

    update auto-deploy.sh

--- shell/auto-deploy.sh
+++ shell/auto-deploy.sh
@@ -154,8 +154,8 @@ kube::config_docker()
     # sysctl net.bridge.bridge-nf-call-iptables=1
     # sysctl net.bridge.bridge-nf-call-ip6tables=1
 cat >>/etc/sysctl.conf <<EOF
-    net.bridge.bridge-nf-call-ip6tables = 1
-    net.bridge.bridge-nf-call-iptables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.bridge.bridge-nf-call-ip6tables = 1
 EOF
 
     apt-mark hold docker-ce
