0bf20b50aef91b4a2d86d2e1a0696d2a474369e8
isaron <isaron@sohu.com>
2018-08-16 16:34:18 +0800

    update shells

--- shell/auto-deploy.sh
+++ shell/auto-deploy.sh
@@ -912,9 +912,9 @@ apiVersion: kubeadm.k8s.io/v1alpha2
 kind: MasterConfiguration
 kubernetesVersion: ${KUBE_VERSION}
 apiServerCertSANs:
-- ${KUBE_VIP}
+- ${LOCAL_IP}
 api:
-    controlPlaneEndpoint: ${KUBE_VIP}:6443
+    controlPlaneEndpoint: ${LOCAL_IP}:6443
 apiServerExtraArgs:
   advertise-address: ${LOCAL_IP}
   disable-admission-plugins: AlwaysPullImages
