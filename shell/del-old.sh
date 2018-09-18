6f817414b9e58bc40ccb46c9477d330d8f866d92
isaron <isaron@sohu.com>
2018-09-18 09:54:27 +0800

    update shell:del-old.sh

--- shell/del-old.sh
+++ shell/del-old.sh
@@ -2,15 +2,15 @@
 
 set -x
 dockerimages=(
-    kube-apiserver-amd64:v1.11.2
-    kube-controller-manager-amd64:v1.11.2
-    kube-scheduler-amd64:v1.11.2
-    kube-proxy-amd64:v1.11.2
-    etcd-amd64:3.1.12
-    pause-amd64:3.1
-    k8s-dns-sidecar-amd64:1.14.10
-    k8s-dns-kube-dns-amd64:1.14.10
-    k8s-dns-dnsmasq-nanny-amd64:1.14.10
+    kube-apiserver-amd64:v1.9.3
+    kube-controller-manager-amd64:v1.9.3
+    kube-scheduler-amd64:v1.9.3
+    kube-proxy-amd64:v1.9.3
+    etcd-amd64:3.1.11
+    pause-amd64:3.0
+    k8s-dns-sidecar-amd64:1.14.7
+    k8s-dns-kube-dns-amd64:1.14.7
+    k8s-dns-dnsmasq-nanny-amd64:1.14.7
     kubernetes-dashboard-amd64:v1.8.3
     cluster-autoscaler:v1.2.2
     defaultbackend:1.4
